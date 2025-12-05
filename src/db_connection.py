14d3a6c1a98fda11e153caae872c73a0ae4e18e0
Christian <chrisvergara7@outlook.com>
2025-12-04 23:11:17 -0600

    fix db exception on debug

--- src/db_connection.py
+++ src/db_connection.py
@@ -4,8 +4,12 @@ import logging
 
 try:
     FLET_APP_STORAGE_DATA = os.getenv("FLET_APP_STORAGE_DATA") # ANDROID MODE
+    conn = sqlite3.connect(os.path.join(FLET_APP_STORAGE_DATA, "financeapp.db"), check_same_thread=False)
+    conn.execute("PRAGMA journal_mode=WAL;")
+
 except Exception as e:
+    FLET_APP_STORAGE_DATA = "/"
+    conn = sqlite3.connect("financeapp.db", check_same_thread=False)
+    conn.execute("PRAGMA journal_mode=WAL;")
     logging.error(f"Error accessing FLET_APP_STORAGE_DATA: {e}", exc_info=True)
 
-conn = sqlite3.connect(os.path.join(FLET_APP_STORAGE_DATA, "financeapp.db"), check_same_thread=False)
-conn.execute("PRAGMA journal_mode=WAL;")
