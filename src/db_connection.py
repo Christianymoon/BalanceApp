3ce3f6969fb54c57b4f8614e5242e2f1f3023217
Christian <chrisvergara7@outlook.com>
2025-12-06 23:46:11 -0600

    DB Connection Simplyfied

--- src/db_connection.py
+++ src/db_connection.py
@@ -1,15 +1,12 @@
 import sqlite3
 import os 
 import logging
+from config import FLET_APP_STORAGE_DATA
 
 try:
-    FLET_APP_STORAGE_DATA = os.getenv("FLET_APP_STORAGE_DATA") # ANDROID MODE
     conn = sqlite3.connect(os.path.join(FLET_APP_STORAGE_DATA, "financeapp.db"), check_same_thread=False)
     conn.execute("PRAGMA journal_mode=WAL;")
+    logging.info(f"Database connection created at {FLET_APP_STORAGE_DATA}")
 
 except Exception as e:
-    FLET_APP_STORAGE_DATA = "/"
-    conn = sqlite3.connect("financeapp.db", check_same_thread=False)
-    conn.execute("PRAGMA journal_mode=WAL;")
-    logging.error(f"Error accessing FLET_APP_STORAGE_DATA: {e}", exc_info=True)
-
+    logging.error(f"Error creating database connection: {e}", exc_info=True)
